05938a510b9f58fe186cb805ef2442d3130ce095
Nika <nikachanto@gmail.com>
2024-04-30 05:14:27 +0400

    04/30/2024
    04/30/2024

---
+++ NewHospital/Models/TimeDataModel.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+
+namespace NewHospital.Models
+{
+    public class TimeDataModel
+    {
+        [Key]
+        public int Id { get; set; } 
+        public string DoctorId { get; set; }
+        public string TimeRange { get; set; }
+
+        public DateTime Day { get; set; }
+        public DateTime StartTime { get; set; } 
+        public DateTime EndTime { get; set; } 
+
+    }
+}
